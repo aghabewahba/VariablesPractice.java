ddef656850840c114bfaf814325a05efb25f5b65
aghabewahba <aghaby69@hotmail.com>
2021-06-01 22:11:38 -0400

    Static Variable and method

--- src/day49_static/StaticVariables.java
+++ src/day49_static/StaticVariables.java
@@ -1,5 +1,10 @@
 package day49_static;
 
 public class StaticVariables {
+    static String name = "Cybertek";
+    static String userName = "admain@cybertek";
+    public static int age =44;
+    public  static double price = 5.99;
+
 
 }
